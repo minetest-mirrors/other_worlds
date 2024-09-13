aaebf80afab149aa1c2d1c069e25b9a49bac8e1a
tenplus1 <kinsellaja@yahoo.com>
2024-09-13 09:34:43 +0100

    tweak and tidy code

--- skybox.lua
+++ skybox.lua
@@ -1,7 +1,10 @@
+
 -- Nether check for Xanadu server
+
 local nether_mod = minetest.get_modpath("nether") and minetest.get_modpath("xanadu")
 
 -- Heights for skyboxes
+
 local underground_low = nether_mod and -28000 or -31000
 local underground_high = -50
 local space_low = 5000
@@ -10,44 +13,47 @@ local redsky_low = 6000
 local redsky_high = 6999
 
 -- Holds name of skybox showing for each player
+
 local player_list = {}
 
 -- Outerspace skybox
+
 local spaceskybox = {
 	"sky_pos_z.png",
 	"sky_neg_z.png^[transformR180",
 	"sky_neg_y.png^[transformR270",
 	"sky_pos_y.png^[transformR270",
 	"sky_pos_x.png^[transformR270",
-	"sky_neg_x.png^[transformR90"
-}
+	"sky_neg_x.png^[transformR90"}
 
 -- Redsky skybox
+
 local redskybox = {
 	"sky_pos_z.png^[colorize:#99000050",
 	"sky_neg_z.png^[transformR180^[colorize:#99000050",
 	"sky_neg_y.png^[transformR270^[colorize:#99000050",
 	"sky_pos_y.png^[transformR270^[colorize:#99000050",
 	"sky_pos_x.png^[transformR270^[colorize:#99000050",
-	"sky_neg_x.png^[transformR90^[colorize:#99000050"
-}
+	"sky_neg_x.png^[transformR90^[colorize:#99000050"}
 
 -- Darkest space skybox
+
 local darkskybox = {
 	"sky_pos_z.png^[colorize:#00005070",
 	"sky_neg_z.png^[transformR180^[colorize:#00005070",
 	"sky_neg_y.png^[transformR270^[colorize:#00005070",
 	"sky_pos_y.png^[transformR270^[colorize:#00005070",
 	"sky_pos_x.png^[transformR270^[colorize:#00005070",
-	"sky_neg_x.png^[transformR90^[colorize:#00005070"
-}
+	"sky_neg_x.png^[transformR90^[colorize:#00005070"}
 
 
 -- check for active pova mod
+
 local pova = minetest.get_modpath("pova")
 
 -- gravity helper function
-local set_gravity = function(player, grav)
+
+local function set_gravity(player, grav)
 
 	if pova then
 		pova.add_override(player:get_player_name(), "default", {gravity = grav})
@@ -56,18 +62,13 @@ local set_gravity = function(player, grav)
 	end
 end
 
+-- globalstep function runs every 2 seconds to show appropriate skybox
 
 local timer = 0
 
 minetest.register_globalstep(function(dtime)
 
-	timer = timer + dtime
-
-	if timer < 2 then
-		return
-	end
-
-	timer = 0
+	timer = timer + dtime ; if timer < 2 then return end ; timer = 0
 
 	local name, pos, current
 
@@ -78,6 +79,7 @@ minetest.register_globalstep(function(dtime)
 		current = player_list[name] or ""
 
 		-- this just adds nether background for xanadu server
+
 		if nether_mod and pos.y < underground_low and current ~= "nether" then
 
 			player:set_sky({
@@ -107,15 +109,11 @@ minetest.register_globalstep(function(dtime)
 			end
 
 		-- Underground (above Nether limit)
+
 		elseif pos.y >= underground_low and pos.y <= underground_high
 		and current ~= "underground" then
 
-			player:set_sky({
-				type = "plain",
-				clouds = false,
-				base_color = "#101010"
-			})
-
+			player:set_sky({type = "plain", clouds = false, base_color = "#101010"})
 			player:set_moon({visible = false})
 			player:set_stars({visible = false})
 			player:set_sun({visible = false, sunrise_visible = false})
@@ -127,14 +125,11 @@ minetest.register_globalstep(function(dtime)
 			end
 
 		-- Earth
+
 		elseif pos.y > underground_high and pos.y < space_low
 		and current ~= "earth" then
 
-			player:set_sky({
-				type = "regular",
-				clouds = true
-			})
-
+			player:set_sky({type = "regular", clouds = true})
 			player:set_moon({visible = true})
 			player:set_stars({visible = true})
 			player:set_sun({visible = true, scale = 1.0, sunrise_visible = true})
@@ -146,15 +141,11 @@ minetest.register_globalstep(function(dtime)
 			end
 
 		-- Outerspace
+
 		elseif pos.y >= space_low and pos.y <= space_high
 		and current ~= "space" then
 
-			player:set_sky({
-				type = "skybox",
-				textures = spaceskybox,
-				clouds = false
-			})
-
+			player:set_sky({type = "skybox", textures = spaceskybox, clouds = false})
 			player:set_moon({visible = false})
 			player:set_stars({visible = false})
 			player:set_sun({visible = true, scale = 1.0, sunrise_visible = false})
@@ -166,15 +157,11 @@ minetest.register_globalstep(function(dtime)
 			end
 
 		-- Redsky
+
 		elseif pos.y >= redsky_low and pos.y <= redsky_high
 		and current ~= "redsky" then
 
-			player:set_sky({
-				type = "skybox",
-				textures = redskybox,
-				clouds = false
-			})
-
+			player:set_sky({type = "skybox", textures = redskybox, clouds = false})
 			player:set_moon({visible = false})
 			player:set_stars({visible = false})
 			player:set_sun({visible = true, scale = 0.5, sunrise_visible = false})
@@ -186,14 +173,10 @@ minetest.register_globalstep(function(dtime)
 			end
 
 		-- Everything else above (the blackness)
+
 		elseif pos.y > redsky_high and current ~= "blackness" then
 
-			player:set_sky({
-				type = "skybox",
-				textures = darkskybox,
-				clouds = false
-			})
-
+			player:set_sky({type = "skybox", textures = darkskybox, clouds = false})
 			player:set_moon({visible = false})
 			player:set_stars({visible = true})
 			player:set_sun({visible = true, scale = 0.1})
@@ -207,10 +190,8 @@ minetest.register_globalstep(function(dtime)
 	end
 end)
 
+-- remove player from list when they leave
 
 minetest.register_on_leaveplayer(function(player)
 	player_list[player:get_player_name()] = nil
 end)
-
-
-print("[MOD] Other Worlds loaded")
